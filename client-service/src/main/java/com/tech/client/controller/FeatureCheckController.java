bc8fece7f51a48b9cf1a1fea462cd18799ba1aba
Medhansh-32 <sharmamedhansh21@gmail.com>
2026-02-22 00:14:47 +0530

    Android Versioning Added

--- client-service/src/main/java/com/tech/client/controller/FeatureCheckController.java
+++ client-service/src/main/java/com/tech/client/controller/FeatureCheckController.java
@@ -1,7 +1,7 @@
 package com.tech.client.controller;
 
 
-import com.tech.client.FeatureCheckService;
+import com.tech.client.service.FeatureCheckService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
